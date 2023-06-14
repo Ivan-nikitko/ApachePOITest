9dc643d9c424bd46bb5f584505ad1926e47283c3
Ivan Nikitka <70625960+Ivan-nikitko@users.noreply.github.com>
2023-06-14 09:32:31 +0200

    refactoring

--- src/main/java/org/example/ExcelExporter.java
+++ src/main/java/org/example/ExcelExporter.java
@@ -3,6 +3,8 @@ package org.example;
 import com.nhl.dflib.DataFrame;
 import com.nhl.dflib.Series;
 import com.nhl.dflib.row.RowProxy;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.ss.formula.FormulaParser;
 import org.apache.poi.ss.formula.FormulaRenderer;
 import org.apache.poi.ss.formula.FormulaType;
@@ -15,6 +17,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFEvaluationWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFTable;
 import org.apache.poi.xssf.usermodel.XSSFTableColumn;
@@ -24,67 +27,14 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 public class ExcelExporter {
-//
-//    public void export(String filePath, String sheetName, String tableName, List<Artist> artists) {
-//
-//        try (FileInputStream inputStream = new FileInputStream(filePath);) {
-//            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-//            XSSFSheet sheet = workbook.getSheet(sheetName);
-//
-//            XSSFTable table = sheet.getTables()
-//                    .stream()
-//                    .filter(t -> t.getName().equals(tableName))
-//                    .findFirst()
-//                    .orElse(null);
-//
-//            if (table != null) {
-//
-//
-//                int startColIndex = table.getStartColIndex();
-//                int startRowIndex = table.getStartRowIndex();
-//
-//                int currentColIndex = startColIndex;
-//                int currentRowIndex = startRowIndex;
-//
-//                for (Artist artist : artists) {
-//
-//                    String formula = sheet.getRow(startRowIndex + 1).getCell(startColIndex + 3).getCellFormula();
-//
-//                    Row dataRow = sheet.createRow(sheet.getLastRowNum());
-//
-//                    Cell cell1 = dataRow.createCell(startColIndex);
-//                    cell1.setCellValue(artist.getId());
-//                    cell1.setCellStyle(getCellStyle(sheet, startColIndex++, startRowIndex));
-//
-//                    Cell cell2 = dataRow.createCell(startColIndex);
-//                    cell2.setCellValue(artist.getArtistName());
-//                    cell2.setCellStyle(getCellStyle(sheet, startColIndex++, startRowIndex));
-//
-//                    Cell cell3 = dataRow.createCell(startColIndex);
-//                    cell3.setCellValue(artist.getDateOfBirth());
-//                    cell3.setCellStyle(getCellStyle(sheet, startColIndex++, startRowIndex));
-//
-//                    Cell cell4 = dataRow.createCell(startColIndex);
-//                    //  String newFormula = copyFormula(sheet, formula, 0, i+1);
-//                    cell4.setCellValue(formula);
-//                    cell4.setCellStyle(getCellStyle(sheet, startColIndex, startRowIndex));
-//                }
-//            }
-//
-//            try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
-//                workbook.write(outputStream);
-//            }
-//            System.out.println("Data exported");
-//
-//        } catch (Exception e) {
-//            System.out.println("Error exporting data: " + e.getMessage());
-//        }
-//
-//    }
+    public ExcelExporter() {
+    }
 
     public void export(String filePath, String tableName, DataFrame dataFrame) {
 
@@ -101,19 +51,11 @@ public class ExcelExporter {
                     .orElse(null);
 
             if (table != null) {
-                int startColIndex = table.getStartColIndex();
-                int startRowIndex = table.getStartRowIndex();
-//
-//                int currentColIndex = startColIndex;
-//                int currentRowIndex = startRowIndex;
-
 
                 List<RowProxy> rowProxies = new ArrayList<>();
                 dataFrame.iterator().forEachRemaining(rowProxies::add);
 
 
-//                String table1 = Printers.tabular.toString(dataFrame);
-//                System.out.println(table1);
                 Iterator<RowProxy> iterator = dataFrame.iterator();
 
 //                while (dataFrame.iterator().hasNext()) {
@@ -122,58 +64,32 @@ public class ExcelExporter {
 //
 //                }
                 List<XSSFTableColumn> columns = table.getColumns();
+                int startColIndex = table.getStartColIndex();
+                int startRowIndex = table.getStartRowIndex();
+
+                Map<String, XSSFCellStyle> columnStylesMap = getCellStyleMap(sheet, columns, startColIndex, startRowIndex);
+
+
                 table.setDataRowCount(rowProxies.size());
 
-
-                List<String> frameColumnsNames = Arrays.asList(dataFrame.getColumnsIndex().getLabels());
+                List<String> frameColumnsNames = getFrameColumnsNames(dataFrame);
 
                 for (int i = 0; i < rowProxies.size(); i++) {
                     Row dataRow = sheet.createRow(sheet.getLastRowNum() + 1);
                     for (XSSFTableColumn tableColumn : columns) {
                         Cell cell = dataRow.createCell(table.getStartColIndex() + tableColumn.getColumnIndex());
                         if (frameColumnsNames.contains(tableColumn.getName())) {
-                            Series<Object> frameColumn = dataFrame.getColumn(tableColumn.getName());
-                            cell.setCellValue(frameColumn.get(i).toString());
+                            processMappedColumn(dataFrame, i, tableColumn, cell);
                         } else {
-                            XSSFCell formulaCandidateCell = sheet.getRow(startRowIndex + 1).getCell(startColIndex+tableColumn.getColumnIndex());
-                            if(formulaCandidateCell.getCellType().equals(CellType.FORMULA)){
-                               cell.setCellFormula(formulaCandidateCell.getCellFormula());
-
-                            } else {
-                                cell.setCellValue("Default");
-                            }
+                            processUnmappedColumn(sheet, startColIndex, startRowIndex, tableColumn, cell);
                         }
-                        cell.setCellStyle(getCellStyleFromTemplate(sheet,startColIndex+tableColumn.getColumnIndex(),startRowIndex));
+                        cell.setCellStyle(columnStylesMap.get(tableColumn.getName()));
                     }
                 }
-
-
-                //   }
-
-
-                //           String formula = sheet.getRow(startRowIndex + 1).getCell(startColIndex + 3).getCellFormula();
-//
-//                Row dataRow = sheet.createRow(sheet.getLastRowNum());
-//
-//                Cell cell1 = dataRow.createCell(startColIndex);
-//                cell1.setCellValue(artist.getId());
-//                cell1.setCellStyle(getCellStyle(sheet, startColIndex++, startRowIndex));
-//
-//                Cell cell2 = dataRow.createCell(startColIndex);
-//                cell2.setCellValue(artist.getArtistName());
-//                cell2.setCellStyle(getCellStyle(sheet, startColIndex++, startRowIndex));
-//
-//                Cell cell3 = dataRow.createCell(startColIndex);
-//                cell3.setCellValue(artist.getDateOfBirth());
-//                cell3.setCellStyle(getCellStyle(sheet, startColIndex++, startRowIndex));
-//
-//                Cell cell4 = dataRow.createCell(startColIndex);
-//                //  String newFormula = copyFormula(sheet, formula, 0, i+1);
-//                cell4.setCellValue(formula);
-//                cell4.setCellStyle(getCellStyle(sheet, startColIndex, startRowIndex));
-
+              //  sheet.shiftRows(startRowIndex , lastRowNum, -1);
             }
 
+         //   removeRow(sheet, 6 + 1);
 
             try (FileOutputStream outputStream = new FileOutputStream(filePath)) {
                 workbook.write(outputStream);
@@ -185,12 +101,56 @@ public class ExcelExporter {
         }
     }
 
-
-    private XSSFCellStyle getCellStyleFromTemplate(XSSFSheet sheet, int startColIndex, int startRowIndex) {
-        return sheet.getRow(startRowIndex + 1).getCell(startColIndex).getCellStyle();
+    /**
+     * Remove a row by its index
+     *
+     * @param sheet    a Excel sheet
+     * @param rowIndex a 0 based index of removing row
+     */
+    public void removeRow(XSSFSheet sheet, int rowIndex) {
+        int lastRowNum = sheet.getLastRowNum();
+        if (rowIndex >= 0 && rowIndex < lastRowNum) {
+            sheet.shiftRows(rowIndex + 1, lastRowNum, -1);
+        }
+        if (rowIndex == lastRowNum) {
+            XSSFRow removingRow = sheet.getRow(rowIndex);
+            if (removingRow != null) {
+                sheet.removeRow(removingRow);
+            }
+        }
     }
 
-    private static String copyFormula(XSSFSheet sheet, String formula, int colDiff, int rowDiff) {
+    private void processUnmappedColumn(XSSFSheet sheet, int startColIndex, int startRowIndex, XSSFTableColumn tableColumn, Cell cell) {
+        XSSFCell formulaCandidateCell = sheet.getRow(startRowIndex + 1).getCell(startColIndex + tableColumn.getColumnIndex());
+        if (formulaCandidateCell.getCellType().equals(CellType.FORMULA)) {
+            cell.setCellFormula(formulaCandidateCell.getCellFormula());
+        } else {
+            cell.setCellValue("Default");
+        }
+    }
+
+    private void processMappedColumn(DataFrame dataFrame, int i, XSSFTableColumn tableColumn, Cell cell) {
+        Series<Object> frameColumnValues = dataFrame.getColumn(tableColumn.getName());
+        cell.setCellValue(frameColumnValues.get(i).toString());
+    }
+
+    private List<String> getFrameColumnsNames(DataFrame dataFrame) {
+        return Arrays.asList(dataFrame.getColumnsIndex().getLabels());
+    }
+
+    private Map<String, XSSFCellStyle> getCellStyleMap(XSSFSheet sheet, List<XSSFTableColumn> columns, int startColIndex, int startRowIndex) {
+        Map<String, XSSFCellStyle> columnStylesMap = new HashMap<>();
+
+        for (XSSFTableColumn column : columns) {
+            XSSFCell templateStyleCell = sheet.getRow(startRowIndex + 1).getCell(startColIndex + column.getColumnIndex());
+            XSSFCellStyle cellStyle = templateStyleCell.getCellStyle();
+            columnStylesMap.put(column.getName(), cellStyle);
+        }
+        return columnStylesMap;
+    }
+
+
+    private String copyFormula(XSSFSheet sheet, String formula, int colDiff, int rowDiff) {
 
         XSSFEvaluationWorkbook workbookWrapper =
                 XSSFEvaluationWorkbook.create(sheet.getWorkbook());
